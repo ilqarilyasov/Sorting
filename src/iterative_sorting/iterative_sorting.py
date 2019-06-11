92c8f03cefc99c1f057dfce5cf22e768f050ae4a
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-11 07:37:32 -0500

    Solve and test the selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,23 +1,28 @@
+import sys
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        smallest_element = sys.maxsize
+        smallest_index = i
 
-
-
-        # TO-DO: swap
-
-
-
-
+        # Find smallest one
+        for j in range(i, len(arr)):
+            if arr[j] < smallest_element:
+                smallest_element = arr[j]
+                smallest_index = j
+                
+        # Swap
+        element = arr[i]
+        arr[i] = smallest_element
+        arr[smallest_index] = element
+        
     return arr
 
 
+selection_sort([67, 4, 90, 34, 99, 23, 12])
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
